551bfa8348e9ab5443d0b1d79cba45c7ee8d25b2
Louish80 <huret.louispro@hotmail.com>
2022-01-05 12:54:43 +0100

    clean

--- src/main/java/com/guildedrose/inventoryInteractor/InventoryInteractor.java
+++ src/main/java/com/guildedrose/inventoryInteractor/InventoryInteractor.java
@@ -1,7 +1,6 @@
 package com.guildedrose.inventoryInteractor;
 
 import com.guildedrose.items.Item;
-
 import java.util.ArrayList;
 
 public class InventoryInteractor implements InventoryUpdater, InventoryViewer {
